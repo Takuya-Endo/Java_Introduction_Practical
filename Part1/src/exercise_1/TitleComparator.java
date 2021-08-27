bc9fb92b55aac588104ba08d8cd59c632a6993db
Takuya-Endo <above5purple@gmail.com>
2021-08-27 11:58:56 +0900

    [Update]練習1-2_1

---
+++ Part1/src/exercise_1/TitleComparator.java
@@ -0,0 +1,11 @@
+package exercise_1;
+
+import java.util.Comparator;
+
+public class TitleComparator implements Comparator<Book> {
+	
+	public int compare(Book book1, Book book2) {
+		return -1;
+	}
+
+}
