aee482bee394b039c5d7623fcb06f36460dd2bf9
Takuya-Endo <above5purple@gmail.com>
2021-08-28 07:49:59 +0900

    [Update]練習3-1_4

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/6/7055ffd58807001c1eca8fb7a467f536
@@ -0,0 +1,9 @@
+package exercise_3;
+
+@FunctionalInterface
+
+public interface Func1 {	
+	
+	public abstract boolean process(int number);
+
+}
---
+++ Part1/src/exercise_3/Func2.java
@@ -0,0 +1,9 @@
+package exercise_3;
+
+@FunctionalInterface
+
+public interface Func2 {
+	
+	public String process(int point, String name);
+
+}
