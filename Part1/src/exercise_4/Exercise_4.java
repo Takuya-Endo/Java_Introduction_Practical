26c72c47d7a4c3bd26fae6d6a0ed33fcd18efee4
Takuya-Endo <above5purple@gmail.com>
2021-08-29 10:46:02 +0900

    [Add]練習4-2

--- Part1/src/exercise_4/Exercise_4.java
+++ Part1/src/exercise_4/Exercise_4.java
@@ -4,8 +4,7 @@ public class Exercise_4 {
 	
 	public static void main(String[] args) {
 		
-		//実行時は()に「クラス名.class」を入れる
-		Launcher.launcher(String.class, StartingMethod.E);
+		Launcher.launcher(MemoryEater.class, StartingMethod.E);
 		
 	}
 
