080966e77cdb581eb48a82f53848391a96c289f4
Takuya-Endo <above5purple@gmail.com>
2021-09-11 14:25:26 +0900

    [Update]練習8-3_3

--- Part2/src/exercise_8/Exercise_8_3.java
+++ Part2/src/exercise_8/Exercise_8_3.java
@@ -1,10 +1,30 @@
 package exercise_8;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpClient.Redirect;
+import java.net.http.HttpClient.Version;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Exercise_8_3 {
 	
 	public static void main(String[] args) {
 		
-		
+		try {
+			
+			HttpClient client = HttpClient.newBuilder().version(Version.HTTP_1_1).followRedirects(Redirect.NORMAL).build();
+			HttpRequest request = HttpRequest.newBuilder().uri(URI.create("")).GET().build();
+			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+			String body = response.body();
+			int status = response.statusCode();
+			
+		} catch (InterruptedException e) {
+			System.out.println(e.getMessage());
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
 		
 	}
 	
