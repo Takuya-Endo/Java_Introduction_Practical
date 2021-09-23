2b2a5b7eecccc15d012fc2573a729fc82cfe0edc
Takuya-Endo <above5purple@gmail.com>
2021-09-23 14:48:37 +0900

    [Update]練習10-1_2

--- Part3/src/exercise_10/Exercise_10_1.java
+++ Part3/src/exercise_10/Exercise_10_1.java
@@ -2,9 +2,14 @@ package exercise_10;
 
 public class Exercise_10_1 {
 	
+	String name ;
+	String address;
+	
+	public void addAccount(String owner, int initZandaka) { }
+	
 	public static void main(String[] args) {
 		
-		
+		System.out.println("試験用のメインメソッドです");
 		
 	}
 
