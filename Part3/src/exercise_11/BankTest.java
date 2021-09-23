11913aae275f21da50ed67dba06ea9f29ef502e8
Takuya-Endo <above5purple@gmail.com>
2021-09-23 15:53:30 +0900

    [Update]練習11-1_2

--- Part3/src/exercise_11/BankTest.java
+++ Part3/src/exercise_11/BankTest.java
@@ -4,8 +4,20 @@ public class BankTest {
 	
 	public static void main(String[] args) {
 		
-		
+		getNameTest();
 		
 	}
-
+	
+	public static void getNameTest() {
+		
+		Bank testBank = new Bank();
+		testBank.setName("テスト銀行");
+		String testBankName = testBank.getName();
+		if (!"テスト銀行".equals(testBankName)) {
+			System.out.println("銀行名エラー");
+		} else {
+			System.out.println("テスト正常終了");
+		}
+	}
+	
 }
