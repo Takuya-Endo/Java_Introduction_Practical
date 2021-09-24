a20c425d9b4edf9efac9429ec873659e784d8d13
Takuya-Endo <above5purple@gmail.com>
2021-09-24 09:44:32 +0900

    [Update]練習15-2_4

--- Part4/src/exercise_15/MyLogger.java
+++ Part4/src/exercise_15/MyLogger.java
@@ -1,8 +1,33 @@
 package exercise_15;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class MyLogger {
 	
+	FileWriter fileWriter;
+	
+	public MyLogger() {
+		
+		try {
+			fileWriter = new FileWriter("C:\\Users\\home\\Desktop\\DMMWEBCAMP\\Java\\Java_Introduction_Practical\\Part4\\src\\exercise_15\\dummylog.txt", true);
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			try { fileWriter.close(); } catch (IOException ex) { System.out.println(ex.getMessage()); }
+		}
+		
+	}
+	
 	public void log(String string) {
+		
+		try {
+			fileWriter.write(string);
+			fileWriter.flush();
+			fileWriter.close();
+			System.out.println(string + "と記述しました");
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
 		
 	}
 
