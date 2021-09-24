6e12f744c49069cc3f539852c4003854c0984e86
Takuya-Endo <above5purple@gmail.com>
2021-09-24 09:07:33 +0900

    [Update]練習15-2_2

--- Part4/src/exercise_15/Main.java
+++ Part4/src/exercise_15/Main.java
@@ -1,5 +1,14 @@
 package exercise_15;
 
 public class Main {
+	
+	public static void main(String[] args) {
+		
+		MyLogger logger1 = ;
+		logger1.log("first");
+		MyLogger logger2 = ;
+		logger2.log("second");
+		
+	}
 
 }
