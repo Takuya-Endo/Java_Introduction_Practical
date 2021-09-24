5a247a705c1f74f9abdbb4bcc8766e1c7cf10423
Takuya-Endo <above5purple@gmail.com>
2021-09-24 12:05:00 +0900

    [Update]練習16-1_3

--- Part4/src/exercise_16/CountUpThread.java
+++ Part4/src/exercise_16/CountUpThread.java
@@ -7,12 +7,13 @@ public class CountUpThread extends Thread {
 	public void run() {
 		
 		for (int i = 0; i < 11; i++) {
+			
 			try {
 				TimeUnit.SECONDS.sleep(1);
 			} catch (InterruptedException e) {
 				System.out.println(e.getMessage());
 			}
-			System.out.print(i + "");
+			System.out.print(i + " ");
 		}
 		
 	}
--- Part4/src/exercise_16/Main.java
+++ Part4/src/exercise_16/Main.java
@@ -4,8 +4,12 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		CountUpThread countUpThread = new CountUpThread();
-		countUpThread.start();
+		CountUpThread countUpThread1 = new CountUpThread();
+		countUpThread1.start();
+		CountUpThread countUpThread2 = new CountUpThread();
+		countUpThread2.start();
+		CountUpThread countUpThread3 = new CountUpThread();
+		countUpThread3.start();
 		
 	}
 
