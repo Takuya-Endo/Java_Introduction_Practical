0b7d357df4cb5c150817e286b1adf759b0118e57
Takuya-Endo <above5purple@gmail.com>
2021-09-30 10:51:50 +0900

    [Update]練習10-1_7

--- Part3/src/exercise_10/Exercise_10_1_sub.java
+++ Part3/src/exercise_10/Exercise_10_1_sub.java
@@ -1,5 +1,21 @@
 package exercise_10;
 
+/** Bankクラス
+ * 銀行を表します */
 public class Exercise_10_1_sub {
+	
+	/** 銀行名 */
+	private String name ;
+	/** 銀行の住所 */
+	private String address;
+	
+	public String getName() { return this.name; }
+	public String getAddress() { return this.address; }
+	
+	/** 口座追加メソッド
+	 * 口座名義人と初回残高を引数に取り、新たに口座を追加します。
+	 * @param owner 口座名義人
+	 * @param initZandaka 初回残高 */
+	public void addAccount(String owner, int initZandaka) { }
 
 }
