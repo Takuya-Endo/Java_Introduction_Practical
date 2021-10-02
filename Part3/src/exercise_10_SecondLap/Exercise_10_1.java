f8adc0063fa8c21f17e2bee790495dff20301f36
Takuya-Endo <above5purple@gmail.com>
2021-10-02 10:14:36 +0900

    [Update]練習10-1_10

--- Part3/src/exercise_10_SecondLap/Exercise_10_1.java
+++ Part3/src/exercise_10_SecondLap/Exercise_10_1.java
@@ -5,7 +5,13 @@ public class Exercise_10_1 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		
+		System.out.println("問題番号表示");
 		
+		Exercise_10_1_sub exercise = new Exercise_10_1_sub("練習", "10-1");
+		
+		System.out.println(exercise.getName() + exercise.getNumber());
+		
+		System.out.println("表示完了");
 
 	}
 
--- Part3/src/exercise_12/Main.java
+++ Part3/src/exercise_12/Main.java
@@ -1,17 +1,14 @@
 package exercise_12;
 
-import java.awt.List;
-import java.util.ArrayList;
-
 public class Main {
 	
 	public static void main(String[] args) {
 		
-		List list = new ArrayList();
-		list.add(args[0]);
-		list.add("world");
-		if (args[0] == "hello") new Exception();
-		for (Object s : list) System.out.print(s);
+//		List list = new ArrayList();
+//		list.add(args[0]);
+//		list.add("world");
+//		if (args[0] == "hello") new Exception();
+//		for (Object s : list) System.out.print(s);
 		
 	}
 
