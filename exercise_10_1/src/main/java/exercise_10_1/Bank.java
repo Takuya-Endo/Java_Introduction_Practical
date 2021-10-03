7826cc8e94018ac1b42f9d8db5ba91176997fe85
Takuya-Endo <above5purple@gmail.com>
2021-10-03 11:39:16 +0900

    [Add]練習10-1_1(SecondLap)

---
+++ exercise_10_1/src/main/java/exercise_10_1/Bank.java
@@ -0,0 +1,16 @@
+package exercise_10_1;
+
+public class Bank {
+	
+	String name;
+	String address;
+	
+	public void addAccount(String owner, int initZandaka) {}
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		System.out.println("試験用のメインメソッドです");
+	}
+
+}
