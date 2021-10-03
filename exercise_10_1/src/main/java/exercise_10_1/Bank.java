37ce0437fd336cfd0045d22bc7fc803979786247
Takuya-Endo <above5purple@gmail.com>
2021-10-03 11:47:42 +0900

    [Update]練習10-1_2(SecondLap)

--- exercise_10_1/src/main/java/exercise_10_1/Bank.java
+++ exercise_10_1/src/main/java/exercise_10_1/Bank.java
@@ -1,10 +1,18 @@
 package exercise_10_1;
 
+
+/** 銀行クラス */
 public class Bank {
 	
+	/** 銀行名 */
 	String name;
+	/** 銀行の住所 */
 	String address;
 	
+	/** 口座作成メソッド
+	 * 	名義人と残高を引数に取り、新たに鋼材を追加します。
+	 * @param owner 口座名義人
+	 * @param initZandaka 新規口座の初期残高*/
 	public void addAccount(String owner, int initZandaka) {}
 
 	public static void main(String[] args) {
