fe49087154a578dc67972a348fdf68cad016eb05
Takuya-Endo <above5purple@gmail.com>
2021-10-07 04:27:23 +0900

    [Update]練習11-sample_2(SecondLap)

--- exercise_11_sample/src/main/java/exercise_11_sample/AccountTest.java
+++ exercise_11_sample/src/main/java/exercise_11_sample/AccountTest.java
@@ -1,5 +1,11 @@
 package exercise_11_sample;
 
 public class AccountTest {
+	
+	@Test public void instantiate() {
+		Account a = new Account("ミナト", 30000);
+		assertEquals("ミナト", a.owner);
+		assertEquals(30000, a.zandaka);
+	}
 
 }
