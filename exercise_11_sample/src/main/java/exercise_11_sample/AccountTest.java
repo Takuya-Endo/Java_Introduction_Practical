6b4efce060658ba88829c4e0836ae4d09ef6c675
Takuya-Endo <above5purple@gmail.com>
2021-10-08 02:00:23 +0900

    [Update]練習11-sample_3(SecondLap)

--- exercise_11_sample/src/main/java/exercise_11_sample/AccountTest.java
+++ exercise_11_sample/src/main/java/exercise_11_sample/AccountTest.java
@@ -2,10 +2,12 @@ package exercise_11_sample;
 
 public class AccountTest {
 	
-	@Test public void instantiate() {
-		Account a = new Account("ミナト", 30000);
-		assertEquals("ミナト", a.owner);
-		assertEquals(30000, a.zandaka);
+	public String owner;
+	public int zandaka;
+	
+	public AccountTest(String owner, int zandaka) {
+		this.owner = owner;
+		this.zandaka = zandaka;
 	}
-
+	
 }
---
+++ exercise_11_sample/src/test/java/exercise_11_sample/SampleTest.java
@@ -0,0 +1,20 @@
+package exercise_11_sample;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SampleTest {
+	
+	@Test public void instantiate() {
+		AccountTest a = new AccountTest("ミナト", 30000);
+		assertEquals("ミナト", a.owner);
+		assertEquals(30000, a.zandaka);
+	}
+	
+	@Test
+	void test() {
+		fail("まだ実装されていません");
+	}
+
+}
